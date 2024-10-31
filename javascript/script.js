65ab4a1355ec40c599750f96f37a6baadc1f12c0
s-kon-field <s-kon@lib.hokudai.ac.jp>
2024-10-31 10:47:09 +0900

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -21,3 +21,18 @@ document.getElementById('prev').addEventListener('click', () => {
 
 // 初期表示
 updateCarousel();
+
+//ハンバーガーメニュー
+$(function () {
+    var $body = $('.gnav_wrapper,.menu-btn');
+
+    //開閉用ボタンをクリックでクラスの切替え
+    $('#js_btn').on('click', function () {
+        $body.toggleClass('open');
+    });
+
+    //メニュー名以外の部分をクリックで閉じる
+    $('a').on('click', function () {
+        $body.removeClass('open');
+    });
+});
